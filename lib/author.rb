0a28f68a5494943cf843eb2e34b9b007f40f0d90
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 02:55:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,5 @@
 class Author
   attr_accessor :name
-  @@post_count = 0
   
   def initialize(name)
     @name = name
@@ -12,15 +11,11 @@ class Author
   
   def add_post(post)
     post.author = self
-    
-    @@post_count += 1
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
-    
-    @@post_count +=1
   end
   
   def self.post_count
