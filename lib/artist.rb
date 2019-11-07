fdd6f3501ed6b8ba802d5cd25fec05d692f089a7
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-07 02:56:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,11 @@ class Artist
   end
   
   def add_song(song)
-    self.songs << song
     song.artist = self
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
-    self.songs << song
     song.artist = self
   end
   
